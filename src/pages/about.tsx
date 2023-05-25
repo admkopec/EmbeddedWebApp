5a699cd557d9043e9c60df32e1fa672757b89b45
szmarkiewicz <markiewiczszym@gmail.com>
2023-05-25 13:30:00 +0200

    Add chakraui to about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,15 @@
 import RootLayout from "@/components/Layout";
+import {ChakraProvider, Text} from "@chakra-ui/react";
+import React from "react";
 
 const AboutPage = () => (
+  <ChakraProvider>
     <RootLayout>
+        <Text>
+            Here goes README
+        </Text>
     </RootLayout>
+  </ChakraProvider>
 );
 
 export default AboutPage;
