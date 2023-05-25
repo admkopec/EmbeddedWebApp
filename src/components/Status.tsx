463e8320a7c57db6665450544109acb1e722d87f
szmarkiewicz <markiewiczszym@gmail.com>
2023-05-25 13:22:13 +0200

    Implement status tab structure

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -1,20 +1,31 @@
-import {Container, Grid, GridItem} from "@chakra-ui/react";
+import {Container, FormControl, FormLabel, Grid, GridItem, Stack, Switch, Text} from "@chakra-ui/react";
 
 const Status = () => (
-  <Grid gap={4} templateAreas={`"act image"
-                                "act_stat image"`}>
+  <Grid gap={4} templateAreas={`"act image"`}>
     <GridItem area={'act'}>
-      <Container>
-        Actuators go here
-      </Container>
-    </GridItem>
-    <GridItem area={'act_stat'}>
-      <Container>
-        Setting their states
+      <Container borderWidth='1px' borderRadius='lg'>
+        <Text>Actuators status</Text>
+        <Stack align={"center"} direction={"row"} alignItems={"center"}>
+          <FormControl display='flex' alignItems='center'>
+            <FormLabel htmlFor='light' mb='0'>
+              Light
+            </FormLabel>
+            <Switch id='light' />
+          </FormControl>
+          <FormControl display='flex' alignItems='center'>
+            <FormLabel htmlFor='bar' mb='0'>
+              Bar
+            </FormLabel>
+            <Switch id='bar' />
+          </FormControl>
+        </Stack>
+        <Text>
+          Click on the switches to change the current status of an actuator.
+        </Text>
       </Container>
     </GridItem>
     <GridItem area={'image'}>
-      <Container>
+      <Container borderWidth='1px' borderRadius='lg' p={2}>
         Current image go here
       </Container>
     </GridItem>
